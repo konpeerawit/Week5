999f590eb9912772af80f4cdd2190763e10c3c9b
Kon-NB <Kon-NB@DESKTOP-5MGOQA6>
2017-09-08 10:21:22 +0700

    Week05P01

--- Week05/Week05/Program.cs
+++ Week05/Week05/Program.cs
@@ -6,6 +6,22 @@ namespace Week05
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Line 1");
+            goto line4;
+            line2:
+            Console.WriteLine("Line 2");
+            goto line9;
+            Console.WriteLine("Line 3");
+            line4:
+            Console.WriteLine("Line 4");
+            Console.WriteLine("Line 5");
+            goto line2;
+            Console.WriteLine("Line 6");
+            Console.WriteLine("Line 7");
+            Console.WriteLine("Line 8");
+            line9:
+            Console.WriteLine("Line 9");
+
         }
     }
 }
