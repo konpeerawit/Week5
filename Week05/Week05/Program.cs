008971c80035f94b7b502deca3d87e01588d1fba
Kon-NB <Kon-NB@DESKTOP-5MGOQA6>
2017-09-08 11:47:44 +0700

    Week05P06.1

--- Week05/Week05/Program.cs
+++ Week05/Week05/Program.cs
@@ -3,12 +3,10 @@ public class TryCatch
 {
     static void Main(string[] args)
     {
-        int value = 800000000;
-        checked // check for overflow
-        {
-            int square = value * value;
-            Console.WriteLine("{0} ^ 2 = {1}", value, square);
-        }
+        int a = int.MaxValue;
+        a *= 2;
+        Console.WriteLine(a);
+        
     }
 }
 
